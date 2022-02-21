3b98920798ed63e875fa20c6767dabcc58de2f46
matty21370 <matthewburke01@hotmail.co.uk>
2022-02-21 16:36:20 +0000

    Backend password hashing

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,36 +1,52 @@
 const router = require('express').Router();
 const User = require('../model/user');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 router.get('/test', (req, res) => {
     res.send("Hello");
 });
 
 router.post('/register', (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    });
+    bcrypt.hash(req.body.password, saltRounds, (err, hashed) => {
+        if(err) {
+            res.send(err);
+        } else {
+            const user = new User({
+                username: req.body.username,
+                password: hashed
+            });
 
-    try {
-        user.save();
-        res.send(user);
-    } catch {
-        res.sendStatus(400);
-    }
+            try {
+                user.save();
+                res.send(user);
+            } catch {
+                res.sendStatus(400);
+            }
+        }
+    });
 });
 
 router.post('/login', (req, res) => {
     const userName = req.body.username;
     const password = req.body.password;
 
-    User.findOne({username: userName, password: password}, (err, user) => {
-        if(err) {
-            res.sendStatus(400);
-            res.send(err);
-        } else {
-            res.send(user);
-        }
-    });
+    if(userName && password) {
+        User.findOne({username: userName}, (err, user) => {
+            if(err) {
+                res.send(err);
+            } else {
+                bcrypt.compare(password, user.password, (err, result) => {
+                    if(result === true) {
+                        res.send(user);
+                    } else {
+                        res.send("Invalid password");
+                    }
+                });
+            }
+        });
+    }
 });
 
 module.exports = router;
