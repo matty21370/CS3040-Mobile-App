0792be22ae84de4cbb98707e3cf3ed43110fc29a
matty21370 <matthewburke01@hotmail.co.uk>
2022-02-21 14:20:23 +0000

    Backend initial commit

---
+++ app/src/main/java/com/matty/mobileapp/HomeActivity.java
@@ -0,0 +1,7 @@
+package com.matty.mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/matty/mobileapp/MainActivity.java
+++ app/src/main/java/com/matty/mobileapp/MainActivity.java
@@ -3,12 +3,17 @@ package com.matty.mobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.btnLogin);
     }
 }
